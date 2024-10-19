ffd0787ee3efe5d7a19429a6947974da5dabd057
rafael.targino <rafaeltargino@gmail.com>
2024-10-19 00:46:39 -0300

    teste main

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 # criando um site seguindo esse tutorial
 #https://pages.hashtagtreinamentos.com/serie-criacaosites-flask-python?blog=1n4033rer&video=3dep762tr
 
-from flask import Flask
+#depois ler isso aqui
+#https://medium.com/@moraneus/python-flask-a-comprehensive-guide-from-basic-to-advanced-fbc6ec9aa5f7
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -12,11 +15,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-    return "Hello, world!"
+    return render_template("index.html")
 
-@app.route("/")
-def homepage():
-    return "Hello, world!"
+@app.route("/user/<username>")
+def users(username):
+    return render_template("user.html", username=username)
 
 
 #colocar o site no ar
